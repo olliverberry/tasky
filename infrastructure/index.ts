5c1a650b0a42c8fbf79393dbe8c573ae5ea595da
Stuart Mehrens <stuartmehrens@Stuarts-MacBook-Pro.local>
2025-05-17 01:08:24 -0400

    add k8s access policy

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,6 +2,7 @@ import { mongo } from "./mongodb/mongo";
 import { eksCluster } from "./k8s/eks";
 import * as s3 from "./s3/s3";
 import * as k8s from "./k8s/apps/server";
+import * as k8sIam from "./k8s/iam";
 
 export const mongoPublicDns = mongo.publicDns;
 export const k8sConfig = eksCluster.kubeconfigJson;
@@ -10,3 +11,5 @@ export const apiServerUrl = eksCluster.eksCluster.endpoint;
 export const s3Url = s3.s3Url;
 export const appNamespace = k8s.appNamespace;
 export const appAdminSa = k8s.appAdminSa;
+export const adminAccess = k8sIam.adminAccess;
+export const adminAccessPolicy = k8sIam.adminAccessPolicy;
--- infrastructure/k8s/iam.ts
+++ infrastructure/k8s/iam.ts
@@ -2,17 +2,22 @@ import * as aws from "@pulumi/aws";
 import { eksCluster } from "./eks";
 import * as config from "../configuration/config";
 
-new aws.eks.AccessEntry("adminAccess", {
+const adminAccess = new aws.eks.AccessEntry("adminAccess", {
   clusterName: eksCluster.core.cluster.name,
   principalArn: config.kubernetes.adminRoleArn!,
   type: "STANDARD",
 });
 
-new aws.eks.AccessPolicyAssociation("adminAccessPolicy", {
-  clusterName: eksCluster.core.cluster.name,
-  principalArn: config.kubernetes.adminRoleArn!,
-  accessScope: {
-    type: "cluster",
-  },
-  policyArn: config.kubernetes.adminPolicyArn!,
-});
+const adminAccessPolicy = new aws.eks.AccessPolicyAssociation(
+  "adminAccessPolicy",
+  {
+    clusterName: eksCluster.core.cluster.name,
+    principalArn: config.kubernetes.adminRoleArn!,
+    accessScope: {
+      type: "cluster",
+    },
+    policyArn: config.kubernetes.adminPolicyArn!,
+  }
+);
+
+export { adminAccess, adminAccessPolicy };
